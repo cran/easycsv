71acae0766076b87450e210a29d340b9c2eaa06d
Dror Bogin <dror.bogin@gmail.com>
2017-08-08 03:16:55 +0000

    version 1.0.1

---
+++ R/loadcsv_multi.R
@@ -0,0 +1,44 @@
+#' @importFrom stats setNames
+#' @importFrom utils read.csv unzip
+#' @export
+loadcsv_multi <- function(directory = NULL,
+                          txt = FALSE,
+                          encoding = "Latin-1",
+                          stringsAsFactors = FALSE,
+                          header = TRUE,
+                          quote = "\"",
+                          fill = TRUE,
+                          comment.char = ""){
+
+  if(is.null(directory)){
+    os <- .Platform
+
+    if(os$OS.type == "windows"){
+      directory <- utils::choose.dir()
+    }else{
+      stop("Please supply a valid local directory")
+    }
+
+  }
+
+  directory = paste(gsub(pattern = "\\", "/", directory,
+                         fixed = TRUE))
+  ending = ifelse(txt == TRUE,
+                  "*.txt$",
+                  "*.csv$")
+    temppath = paste(directory,list.files(path = directory, pattern=ending), sep = "/")
+    tempfiles = list.files(path = directory, pattern=ending)
+    list2env(
+        lapply(setNames(temppath,
+                        make.names(paste0(gsub(ending, "", tempfiles)))),
+               read.csv,
+               encoding = encoding,
+               stringsAsFactors = stringsAsFactors,
+               header = header,
+               quote = quote,
+               fill = fill,
+               comment.char = comment.char),
+        envir = .GlobalEnv)
+
+
+}
